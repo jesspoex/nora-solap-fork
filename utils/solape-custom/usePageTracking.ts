f296903307f2dab3cbb8279f1b5c818defdf5468
Prett <41442866+jesspoex@users.noreply.github.com>
2023-10-25 16:39:56 +0700

    Update usePageTracking.ts

--- utils/solape-custom/usePageTracking.ts
+++ utils/solape-custom/usePageTracking.ts
@@ -1,4 +1,4 @@
-export {/*
+export {
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
@@ -24,4 +24,4 @@ const usePageTracking = () => {
 
 export default usePageTracking;
 
-*/}
+}
